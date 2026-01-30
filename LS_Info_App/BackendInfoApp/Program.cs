0496b247c0135595afb6906405c5ed27dc818c06
TsukiYami <tsukiyamigaming@gmail.com>
2026-01-30 20:36:47 +0100

    Changing from gRPC to Rest

--- LS_Info_App/BackendInfoApp/Program.cs
+++ LS_Info_App/BackendInfoApp/Program.cs
@@ -1,16 +1,23 @@
-using BackendInfoApp.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddGrpc();
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/",
-    () =>
-        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
